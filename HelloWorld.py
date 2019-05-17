a735f8056487c0f33f558cc2d7714bf9dfb20432
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-17 15:29:46 +0200

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,5 @@
+import random
+
 print("Hello World\n")
 print("Yes, that's it... ")
 
@@ -6,3 +8,13 @@ print(sorted(t))
 
 my_list = [1, 2, 3, 4, 5]
 print(my_list[2], my_list[2:2], my_list[2:3])
+
+
+data = [random.randint(0, 100) for i in range(10)]
+average = sum(data)//len(data)
+
+print(data, average)
+if data[len(data)//2] > average:
+    print(True)
+else:
+    print(False)
